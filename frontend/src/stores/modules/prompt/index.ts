667f5d9ee9f2600d0dc3493057f069e5456d697a
常雲翔 <103975914+supercyx3@users.noreply.github.com>
2023-06-24 19:12:55 +0800

    细节助手

--- frontend/src/stores/modules/prompt/index.ts
+++ frontend/src/stores/modules/prompt/index.ts
@@ -27,7 +27,7 @@ export const usePromptStore = defineStore(
         type: 1,
         name: 'ChatGPT 中文调教指南 - 简体',
         url: './data/prompts/prompts-zh.json',
-        refer: 'https://github.com/PlexPt/awesome-chatgpt-prompts-zh',
+        refer: 'https://github.com/supercyx3/awesome-chatgpt-prompts-zh',
       },
       {
         type: 1,
